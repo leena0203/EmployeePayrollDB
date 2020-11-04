7f15ee6775a7e8da974ec5d996e679d8a5d5eea0
leena0203 <leenasarode02@gmail.com>
2020-11-04 21:01:05 +0530

    Added: Execute acc. new table all UC

--- src/test/java/EmployeeDB/EmpPayrollTest.java
+++ src/test/java/EmployeeDB/EmpPayrollTest.java
@@ -19,59 +19,59 @@ public class EmpPayrollTest {
 		test = new EmployeePayrollService();
 	    testData = test.readEmployeePayrollData(IOService.DB_IO);
 	}
-//	//UC2
-//	@Test
-//	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-//		assertEquals(3, testData.size());
-//	}
-//
-//	//UC3 and UC4
-//	@Test
-//	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncDatabase() {
-//	    test.updateEmployeeSalary("Leena", 3000000.00);
-//	    boolean result = test.checkEmployeePayrollInSyncWithDBI("Leena");
-//		assertTrue(result);
-//	}
-//
-//	//UC5
-//	@Test
-//	public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
-//	    LocalDate start = LocalDate.of(2019, 9, 01);
-//	    LocalDate end = LocalDate.now();
-//	    testData = test.readEmployeePayrollForDateRange(start, end);
-//	    assertEquals(2, testData.size());
-//	}
-//	//UC6
-//	@Test
-//	public void givenPayrollData_WhenAvgSalaryRetrievedByGender_ShouldReturnProperValue() {
-//	    Map<String, Double> genderBasedFunctions = test.readAvgSalaryByGender();
-//	    assertTrue(genderBasedFunctions.get("M").equals(5400000.0));
-//	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
-//	}
-//	@Test
-//	public void givenPayrollData_WhenMinSalaryRetrievedByGender_ShouldReturnProperValue() {
-//	    Map<String, Double> genderBasedFunctions = test.readMinSalaryByGender();
-//	    assertTrue(genderBasedFunctions.get("M").equals(1200000.0));
-//	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
-//	}
-//	@Test
-//	public void givenPayrollData_WhenMaxSalaryRetrievedByGender_ShouldReturnProperValue() {
-//	    Map<String, Double> genderBasedFunctions = test.readMaxSalaryByGender();
-//	    assertTrue(genderBasedFunctions.get("M").equals(9000000.0));
-//	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
-//	}
-//	@Test
-//	public void givenPayrollData_WhenSumRetrievedByGender_ShouldReturnProperValue() {
-//	    Map<String, Double> genderBasedFunctions = test.readSumByGender();
-//	    assertTrue(genderBasedFunctions.get("F").equals( 3000000.0));
-//	    assertTrue(genderBasedFunctions.get("M").equals(10200000.0)); 
-//	    }
-//	@Test
-//	public void givenPayrollData_WhenCountRetrievedByGender_ShouldReturnProperValue() {
-//	    Map<String, Double> genderBasedFunctions = test.readCountByGender();
-//	    assertTrue(genderBasedFunctions.get("M").equals(2.0));
-//	    assertTrue(genderBasedFunctions.get("F").equals(1.0));
-//	}
+	//UC2
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+		assertEquals(3, testData.size());
+	}
+
+	//UC3 and UC4
+	@Test
+	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncDatabase() {
+	    test.updateEmployeeSalary("Leena", 3000000.00);
+	    boolean result = test.checkEmployeePayrollInSyncWithDBI("Leena");
+		assertTrue(result);
+	}
+
+	//UC5
+	@Test
+	public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
+	    LocalDate start = LocalDate.of(2019, 9, 01);
+	    LocalDate end = LocalDate.now();
+	    testData = test.readEmployeePayrollForDateRange(start, end);
+	    assertEquals(2, testData.size());
+	}
+	//UC6
+	@Test
+	public void givenPayrollData_WhenAvgSalaryRetrievedByGender_ShouldReturnProperValue() {
+	    Map<String, Double> genderBasedFunctions = test.readAvgSalaryByGender();
+	    assertTrue(genderBasedFunctions.get("M").equals(5400000.0));
+	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
+	}
+	@Test
+	public void givenPayrollData_WhenMinSalaryRetrievedByGender_ShouldReturnProperValue() {
+	    Map<String, Double> genderBasedFunctions = test.readMinSalaryByGender();
+	    assertTrue(genderBasedFunctions.get("M").equals(1200000.0));
+	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
+	}
+	@Test
+	public void givenPayrollData_WhenMaxSalaryRetrievedByGender_ShouldReturnProperValue() {
+	    Map<String, Double> genderBasedFunctions = test.readMaxSalaryByGender();
+	    assertTrue(genderBasedFunctions.get("M").equals(9000000.0));
+	    assertTrue(genderBasedFunctions.get("F").equals(3000000.0));
+	}
+	@Test
+	public void givenPayrollData_WhenSumRetrievedByGender_ShouldReturnProperValue() {
+	    Map<String, Double> genderBasedFunctions = test.readSumByGender();
+	    assertTrue(genderBasedFunctions.get("F").equals( 3000000.0));
+	    assertTrue(genderBasedFunctions.get("M").equals(10200000.0)); 
+	    }
+	@Test
+	public void givenPayrollData_WhenCountRetrievedByGender_ShouldReturnProperValue() {
+	    Map<String, Double> genderBasedFunctions = test.readCountByGender();
+	    assertTrue(genderBasedFunctions.get("M").equals(2.0));
+	    assertTrue(genderBasedFunctions.get("F").equals(1.0));
+	}
 	
 	//UC7
 	@Test
@@ -81,12 +81,12 @@ public class EmpPayrollTest {
 	    assertTrue(result);
 	    System.out.println();
 	}
-//	//UC8
-//	@Test
-//	public void givenEmployeePayrollInDB_WhenEmployeeDeleted_ShouldMatchEmployeeCount() {
-//		test.deleteEmployee("Mill");
-//		assertEquals(6, testData.size());
-//	}
+	//UC8
+	@Test
+	public void givenEmployeePayrollInDB_WhenEmployeeDeleted_ShouldMatchEmployeeCount() {
+		test.deleteEmployee("Mill");
+		assertEquals(6, testData.size());
+	}
 	
 	//UC9
 	@Test
